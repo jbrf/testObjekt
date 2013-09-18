96132c84bc1a511cee6de3d54e7c0ef6b4035948
Johan Björklund <johan@jsbmaleri.se>
2013-09-18 15:48:25 +0200

    Some more understanding of the example.. perhaps

--- testObjekt/Program.cs
+++ testObjekt/Program.cs
@@ -3,8 +3,35 @@
 
 class Program
 {
+    public string TillRovarSprak(string text)
+    {
+        char[] chars = text.ToCharArray();
+        string result = "";
+
+        foreach (char achar in chars)
+        {
+            if ("bcdfghjklmnpqrstvxz".IndexOf(achar) >= 0)
+            {
+                // Är konsonant
+            }
+            else
+            {
+                // Är inte konsonant så lägg till i result
+                //result += achar;
+                result = result + achar;
+            }
+        }
+        return result;
+    }
+    
+  
     static void Main()
     {
+        string text = "Hej";
+        
+
+       
+       
         
     }
 }
